8ed54fb051bedf8058a6a984b973cc02e5ef80de
Satvik Gaikwad <satvik.g.code@gmail.com>
2022-10-09 16:52:35 +0530

    Inital Commit

---
+++ 231/A.Team/main.go
@@ -0,0 +1,22 @@
+package main
+import (
+	"fmt"
+)
+func main() {
+	var n int
+	fmt.Scanln(&n)
+	var cn int = 0
+	var a,b,c int
+	for (n>0){
+		fmt.Scan(&a)
+		fmt.Scan(&b)
+		fmt.Scan(&c)
+		if a+b+c > 1 {
+			cn++
+		}
+		n--
+		if n ==0 {
+			fmt.Println(cn)
+		}
+	}
+}
